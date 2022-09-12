00e04793b0eae9c7330c0c223a7c18bd6e1dd839
CalmEnduring <dantelm22@gmail.com>
2022-09-12 07:09:31 -0700

    c14

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
-int main():
+/* Standard header file included */
+#include <stdio.h>
+
+int main(void);
 {
-	puts("Programming is like building a multilingual puzzle"):
-	return(0)
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
