78e04cdf122e959076b95061cd620061b6048dc7
CalmEnduring <dantelm22@gmail.com>
2022-09-16 14:54:09 -0700

    Defeat the infinte loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - causes infinte loop
+  * Return: 0
+  */
+
+int maind(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	
+	/**while (I < 10)
+	  *{
+	  *	putchar(i);
+	  *}
+	  */
+	printf("Infinte loop avoid! \\o/\n");
+
+	return (0);
+}
