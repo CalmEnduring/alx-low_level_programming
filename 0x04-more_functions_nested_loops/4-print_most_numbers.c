4a71cff9129f600ce960204a2dfd77786ee36390
CalmEnduring <dantelm22@gmail.com>
2022-09-20 13:39:19 -0700

    Print numbers with exceptions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - Prints numbers 0-9 with the exception of 2 and 4
+  *
+  * Return: Numbers from 0-9 except 2 and 4
+  */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
