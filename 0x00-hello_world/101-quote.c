4f40ee2af58a372997ff3663c84f109b4a726f72
CalmEnduring <dantelm22@gmail.com>
2022-10-01 14:42:42 -0700

    Return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
 	return (1);
 }
