691517829e6a7659434e39ca597e1e42940d82b2
CalmEnduring <dantelm22@gmail.com>
2022-10-12 14:41:30 -0700

    String Comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * _strcmp - Comapares two strings
+  * @s1: string one
+  * @s2: string two
+  *
+  * Return:	0, if strings are equal
+  *		- value if s1 is less than s2
+  *		+ value if s1 is greather than s2
+  */
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+	{
+		return (0);
+	}
+	else if (s1 < s2)
+	{
+		return (-15);
+	}
+	else
+	{
+		return (15);
+	}
+}
