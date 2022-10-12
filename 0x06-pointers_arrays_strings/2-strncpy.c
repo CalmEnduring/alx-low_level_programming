800086d3df805c44624e6e24879c4491cf513e98
CalmEnduring <dantelm22@gmail.com>
2022-10-12 14:11:28 -0700

    Copy n Elements of string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int index;
 
-	for (index = 0; dest[index] != '\0' && index < n; index++)
+	for (index = 0; *(src + index) && index < n; index++)
 	{
-		dest[index] = src[index];
+		*(dest + index) = *(src + index);
+	}
+	for (; index < n; index++)
+	{
+		*(dest + index) = '\0';
 	}
 
 	return (dest);
