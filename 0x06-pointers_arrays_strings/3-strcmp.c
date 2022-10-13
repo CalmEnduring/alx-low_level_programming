cb2eba5c37683ebdbc1c2178d914b184ffffa61f
CalmEnduring <dantelm22@gmail.com>
2022-10-13 06:00:41 -0700

    String Comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,7 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 == s2)
-	{
-		return (0);
-	}
-	else if (s1 < s2)
-	{
-		return (-15);
-	}
-	else
-	{
-		return (15);
-	}
+	int value = *s1 - *s2;
+
+	return (value);
 }
