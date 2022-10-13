42077620f86a2b52e02316519ac999fb2f83089b
CalmEnduring <dantelm22@gmail.com>
2022-10-13 12:28:00 -0700

    Yes Cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+  * cap_string - Capaitalizes each word in the string
+  * @str: The string in question
+  *
+  * Return: str
+  */
+char *cap_string(char *str)
+{
+	int index1 = 0;
+	int index2;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	while (*(str + index1))
+	{
+		if (*(str + index1) >= 'a' && (*str + index1) <= 'z')
+		{
+			if (index1 == 0)
+			{
+				*(str + index1) -= 32;
+			}
+			else
+			{
+				for (index2 = 0; index2 <= 12; index2++)
+				{
+					if (separators[index2] == *(str + index1 - 1))
+					{
+						*(str + index1) -= 32;
+					}
+				}
+			}
+		}
+		index1++;
+	}
+	return (str);
+}
