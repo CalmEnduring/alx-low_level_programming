b523889c930a38ed50b624f8c6c935654acf3d0c
CalmEnduring <dantelm22@gmail.com>
2022-10-13 06:10:22 -0700

    String Comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,15 @@
   */
 int _strcmp(char *s1, char *s2)
 {
-	int value = *s1 - *s2;
-
-	return (value);
+	for (; *s1 != '\0' && *s2 != '\0' && *s1 == *s2; s1++, s2++)
+	{
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
