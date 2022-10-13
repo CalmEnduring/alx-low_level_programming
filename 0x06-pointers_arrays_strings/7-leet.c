d3c2f56dc01639f2ef013266d8aa6b5ecbf409b7
CalmEnduring <dantelm22@gmail.com>
2022-10-13 14:58:04 -0700

    Leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
   * leet - Replaces specific elements in a string
-  * @str: String in question
+  * @s: String in question
   *
-  * Return: str
+  * Return: s
   */
-char *leet(char *)
+char *leet(char *s)
 {
 	int index1, index2;
 	char rplc1[] = "aAeEoOtTlL";
