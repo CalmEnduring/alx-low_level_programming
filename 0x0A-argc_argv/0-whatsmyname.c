4eeb22339fb8c11594dba4c0c293d9a0a13ceaac
CalmEnduring <dantelm22@gmail.com>
2022-10-25 14:16:06 -0700

    NewName

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * @argc: Argument count
+  * @argv: Arguments
+  *
+  * Return: 0
+  */
+int main(int argc __attribute__((unused)), char *argv[])
+{ /* prints the name of the file */
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
