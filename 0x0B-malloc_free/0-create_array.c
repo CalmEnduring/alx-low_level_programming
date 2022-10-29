df2578b718f6a7ef261c5321a4a6b4212fd34881
CalmEnduring <dantelm22@gmail.com>
2022-10-29 10:41:09 -0700

    Header File and more

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+  * create_array - Creates an initialized array
+  * @size: Size of the array
+  * @c: character to initialize
+  *
+  * Return: c on success, NULL on failure
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *space; /* point to the dynamic storage */
+	unsigned int index;
+
+	space = malloc(size * sizeof(char)); /* size of storage to allocate */
+
+	for (index = 0; index < size; index++) /* loops for each element in array */
+	{
+		if (size == 0) /* if size is 0, terminate with NULL */
+		{
+			return (NULL);
+		}
+		else if (space == NULL) /* failure */
+		{
+			return (NULL);
+		}
+		else /* assigns c to  indexed value */
+		{
+			space[index] = c;
+		}
+	} /* returns a pointer to a string */
+	return (space);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Functions which utilize malloc and free
