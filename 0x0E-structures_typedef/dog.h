299ebba4ae6c91769236c155d9aa9c6378ddd268
CalmEnduring <dantelm22@gmail.com>
2022-11-03 16:12:22 -0700

    Header File + Readme

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+/**
+  * struct dog - info about dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
