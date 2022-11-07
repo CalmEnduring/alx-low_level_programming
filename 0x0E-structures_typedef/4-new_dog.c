8e3259dab959cf0243fbd2e60e447e63be34a832
CalmEnduring <dantelm22@gmail.com>
2022-11-07 04:59:03 -0800

    New Struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,43 +12,39 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	int index, index2, index3;
 	dog_t *ndog;
-	/* function failure */
+
 	if (name == NULL || owner == NULL)
-		return (NULL);
+		return (NULL); /* function failure */
 	/* allocate space for new structure */
 	ndog = malloc(sizeof(dog_t));
 	if (ndog == NULL) /* malloc failure */
 		return (NULL);
-	/* read name and owner */
-	index = 0;
-	while (name[index])
-		index++;
+	for (index = 0; name[index]; index++)
+		;
 	index++; /* increment once more for NULL term */
 	/* allocate space for name */
-	ndog->name = malloc(sizeof(char) * index);
+	ndog->name = malloc(index * sizeof(char));
 	if (ndog->name == NULL)
 	{
 		free(ndog); /* free spaced allocated for structure */
 		return (NULL);
 	}
-	/* loop through each element in name string */
 	for (index3 = 0; index3 < index; index3++)
 	{ /* copy name into name in struct */
 		ndog->name[index3] = name[index3];
 	}
-	while (owner[index])
-		index2++;
-	index2++; /* increment once more for NULL term */	
+	ndog->age = age; /* copy age into age in struct */
+	for (index2 = 0; owner[index2]; index2++)
+		;
+	index2++; /* increment once more for NULL term */
 	/* allocate space for owner */
-	ndog->owner = malloc(sizeof(char) * index2);
+	ndog->owner = malloc(index2 * sizeof(char));
 	if (ndog->owner == NULL) /* malloc failure */
 	{
 		free(ndog->name); /* free space allocated for name in struct */
 		free(ndog); /* free space allocated for struct */
 		return (NULL);
-	}	
-	ndog->age = age; /* copy age into age in struct */
-	/* loop through each element in owner string */
+	}
 	for (index3 = 0; index3 < index2; index3++)
 	{ /* copy owner into owner in struct */
 		ndog->owner[index3] = owner[index3];
