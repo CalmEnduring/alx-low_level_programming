e5a286bfa636c165e5fea4401ce98387da640d31
CalmEnduring <dantelm22@gmail.com>
2022-11-09 08:25:13 -0800

    Free Dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+  * free_dog - frees dynamicaly allocated memory of a struct
+  * @d: pointer to a structure
+  */
+void free_dog(dog_t *d)
+{
+	free((*d).name);
+	free((*d).owner);
+	free(d);
+}
