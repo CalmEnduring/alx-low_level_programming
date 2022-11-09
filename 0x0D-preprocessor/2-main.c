433d3f8b502b068e56695b41b42ef3de429a8a3d
CalmEnduring <dantelm22@gmail.com>
2022-11-09 12:55:39 -0800

    FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - prints name of source file
+  *
+  * Return: Always 0
+  */
+int main(void)
+{/* using predefine macro FILE */
+	printf("%s\n", __FILE__);
+	return (0);
+}
