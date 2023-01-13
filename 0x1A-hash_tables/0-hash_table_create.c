61d545775d1a1615c32109786c479a369326d1fa
CalmEnduring <dantelm22@gmail.com>
2023-01-13 13:31:14 -0800

    create hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/*
+ * hash_table_create - creates a hash table
+ * @size: size of the hash table
+ *
+ * Return: a hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = (hash_table_t*)malloc(sizeof(hash_table_t));
+
+	table->size = size;
+	table->array = NULL;
+
+	return (table);
+}
