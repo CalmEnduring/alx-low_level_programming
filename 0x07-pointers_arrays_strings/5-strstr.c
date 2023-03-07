ce90cbfa1426223962fca129f18556b0bb50d65b
CalmEnduring <dantelm22@gmail.com>
2023-03-07 02:05:01 -0800

    find the needle in the haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * _strstr - locates a substring
+  * @haystack: string to search
+  * @needle: string to locate
+  *
+  * Return: a pointer to the located substring
+  *	or NULL if string was not found
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	/* iterate over the needle */
+	while (*needle)
+	{
+		/* iterate over haystack */
+		while (*haystack)
+		{
+			/* if the needle is in the haystack */
+			if (*needle == *haystack)
+			{
+				/* return the needle in the haystack */
+				return (haystack);
+			}
+			/* move to next position in haystack */
+			haystack++;
+		}
+		/* move to next position in neelde */
+		needle++;
+	}
+	/* if needle not found return NULL */
+	return ('\0');
+}
