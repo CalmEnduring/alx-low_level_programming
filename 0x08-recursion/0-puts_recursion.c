f153e576d931e9bb18ca6a087ef7cd4136b9d1e2
CalmEnduring <dantelm22@gmail.com>
2023-03-09 00:30:39 -0800

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * _puts_recursion - prints a string followed by a newline
+  * @s: string to print
+  */
+void _puts_recursion(char *s)
+{
+	/* recur until NULL terminator */
+	if (*s != '\0')
+	{
+		_putchar(*s); /* print current character */
+		s++; /* move to next character */
+		_puts_recursion(s); /* recur */
+	}
+	_putchar('\n'); /* newline */
+}
