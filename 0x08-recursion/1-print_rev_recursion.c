3e7e2eb534f31acd1bbe71fb7443d5cbb1533248
CalmEnduring <dantelm22@gmail.com>
2023-03-12 05:17:30 -0700

    reverse string recur

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,21 @@
 #include "main.h"
+#include <string.h>
 /**
   * _print_rev_recursion - prints a string in reverse
   * @s: string
   */
 void _print_rev_recursion(char *s)
 {
+
 	/* recur until NULL terminator */
 	if (*s != '\0')
 	{
-		s++; /* move to next character */
-		_print_rev_recursion(s); /* recur */
-		_putchar(*s); /* print character */
+		_print_rev_recursion(s + 1); /* recur */
+		_putchar(*s);
+
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
