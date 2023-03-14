dc2e86eb02a99bcd9aa2186ff31dc8d1c8bfe80c
CalmEnduring <dantelm22@gmail.com>
2023-03-14 06:28:53 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,22 +9,28 @@
   */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *array;
+	unsigned int i; /* iterator */
+	char *array; /* pointer to an array */
 
-
+	/* if size of array is 0 return NULL */
 	if (size == 0)
 		return (NULL);
 
+	/* allocate memory for array */
+	/* size of data type * size of array */
 	array = malloc(sizeof(c) * size);
 
+	/* if array NULL, no space available, return NULL */
 	if (array == NULL)
 		return (NULL);
 
+	/* loop through the array */
 	for (i = 0; i < size; i++)
 	{
+		/* initialize each space with a constant char */
 		array[i] = c;
 	}
+	/* NULL terminate array */
 	array[i] = '\0';
 	return (array);
 }
