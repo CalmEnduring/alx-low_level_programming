68d7b0a8f1475331637aee6cad7820a833bd8901
CalmEnduring <dantelm22@gmail.com>
2023-03-16 04:35:16 -0700

    allocate memory malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,16 +7,19 @@
   */
 void *malloc_checked(unsigned int b)
 {
-	int *space;
+	int *space; /* pointer to allocate memory */
 
-	space = malloc(b);
+	space = malloc(b); /* allocate memory */
 
+	/* if no space available */
 	if (space == NULL)
 	{
+		/* process termination with status val 98 */
 		exit(98);
 	}
 	else
 	{
+		/* otherwise return pointer to allocated memory */
 		return (space);
 	}
 }
