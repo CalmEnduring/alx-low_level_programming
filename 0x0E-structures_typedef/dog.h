c0890510d03189f1b9bb3d4b299adeac8664813d
CalmEnduring <dantelm22@gmail.com>
2023-03-21 00:54:59 -0700

    initialize the dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,6 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-/* dog_t *new_dog(char *name, float age, char *owner); */
-/* void free_dog(dog_t *d); */
-
 /**
   * struct dog - structure that defines a dog
   * @name: name of dog
@@ -18,5 +13,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+/* dog_t *new_dog(char *name, float age, char *owner); */
+/* void free_dog(dog_t *d); */
 
 #endif
