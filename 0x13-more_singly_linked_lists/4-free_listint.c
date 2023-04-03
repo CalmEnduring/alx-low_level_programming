b1fed9c8ec62f94b71c4672a247935205f57a5a8
CalmEnduring <dantelm22@gmail.com>
2023-04-03 06:11:29 -0700

    free linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+  * free_listint - frees a listint_t list
+  * @head: pointer to head node
+  *
+  * Return: void
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *temp; /* pointer to store an address temporarily */
+
+	while (head) /* traverse through list */
+	{
+		temp = head; /* temp holds address of head */
+		head = head->next; /* move to next node */
+		free(temp); /* free current node */
+	}
+	head = NULL; /* indication that head is now empty */
+}
