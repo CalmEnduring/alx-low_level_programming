5eae0c5c37b1a5e6261f2e4d4d3ff5d1a5a93431
CalmEnduring <dantelm22@gmail.com>
2023-04-03 08:04:29 -0700

    sqrt function + header file

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,4 +7,27 @@
   */
 int _sqrt_recursion(int n)
 {
-	if ()
+	return (squarert(n, 1));/* call recursion function */
+}
+
+/**
+  * squarert - calculates the natural square root
+  * @n: number
+  * @i: iterator
+  *
+  * Return: natural square root
+  */
+int squarert(int n, int i)
+{
+	int sr; /* holds the square root */
+
+	sr = i * i; /* initialize to i multiplying itself */
+
+	if (sr > n) /* no natural square root */
+		return (-1);
+
+	if (sr == n) /* natural square root found */
+		return (i); /* return square root */
+
+	return (squarert(n, i + 1)); /* recur */
+}
