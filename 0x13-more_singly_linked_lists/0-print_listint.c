0858c220ed0073679bf105d83175afb020076eaf
CalmEnduring <dantelm22@gmail.com>
2023-04-03 03:35:39 -0700

    header file + printlist function

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+  * print_listint - prints the elements of a listint_t list
+  * @h: list
+  *
+  * Return: number of nodes
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t non = 0; /* number of nodes */
+
+	while (h != NULL) /* traverse through lists */
+	{
+		printf("%d\n", h->n); /* print element */
+		h = h->next; /* move to next node */
+		non++; /* increment number of nodes */
+	}
+	return (non); /* return number of nodes */
+}
