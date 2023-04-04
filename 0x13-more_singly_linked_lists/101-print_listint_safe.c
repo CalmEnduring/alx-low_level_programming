dd7b991929c546ec96bf8579c06244a2daf75565
CalmEnduring <dantelm22@gmail.com>
2023-04-04 16:41:06 -0700

    code build

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,71 +1,50 @@
 #include "lists.h"
 
 /**
-* print_listint_safe - prints a list but safely
-* @head: the head node. pointer
-* 
-* Description: dont wanna print a infinite loop
-* 	either match addresses or free the head so cant loop back
-* Return: the count number
-* A: we make an array that will store everything from head given
-* B: while the head is not null, we iterate through
-* C: count is initially 0 and i is 0 so we dont even loop through the first
-* 	time, we skip this. we assign array to the head and increment head
-* 	then we increase count so when we loop through again, we can go
-* 	into the for loop
-* D: inside the loop, we check if the head value is equal to the array values
-* 	if we found a match then we turn the boolean flag on and break and
-* 	we make the index equal to whatever the i counter was
-* E: if we found a match, which is a loop, we break out of the while loop
-* F: new loop, we will loop through our array values we copied over from head
-* 	and we print out their pointer address and their value.
-* G: if we had an infinite loop we have a flag check of 1 which will
-* 	also print out the index which is the last one.
-*/
-
+ * print_listint_safe - Function that prints a linked list
+ * @head: Beginning of the list
+ * Return: Number of nodes
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	size_t index = 0;
-	listint_t const **array;/* A */
+	unsigned int i = 0, j = 0, k = 0, bool = 0;
+	listint_t *slow, *fast, *temp;
 
-	array = malloc(sizeof(listint_t *) * 1024);
-	if (!array)
-		exit(98);
-	unsigned int i = 0;
-	unsigned int flag = 0;
-
-	while (head != NULL)/* B */
+	if (head == NULL)
+		printf("0\n"), exit(98);
+	slow = (listint_t *)head, fast = (listint_t *)head;
+	while (slow && fast && fast->next)
 	{
-		for (i = 0; i < count; i++)/* C*/
+		slow = slow->next, fast = fast->next->next;
+		if (slow == fast)
 		{
-			if (head == array[i])/* D */
+			bool = 1;
+			break;
+		}
+	}
+	if (bool == 1)
+	{
+		for (j = 1; fast->next != slow; j++)
+			fast = fast->next;
+		slow = (listint_t *)head, fast = (listint_t *)head;
+		for (; k < j; k++)
+			fast = fast->next;
+		while (1)
+		{
+			fast = fast->next, slow = slow->next;
+			if (fast->next == slow->next)
 			{
-				flag = 1;
-				index = i;
+				temp = slow->next, fast->next = NULL;
 				break;
 			}
-			else
-				flag = 0;
 		}
-
-		if (flag == 1)/* E */
-			break;
-		array[count] = head;
-		head = head->next;
-		count++;
 	}
-
-	i = 0;
-	while (i < count)/* F */
+	while (head)
 	{
-		printf("[%p] %d\n", (void *)array[i], array[i]->n);
-		i++;
+		(head == NULL) ? (void)exit(98) : (void)head;
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next, i++;
 	}
-	if (flag == 1)/* G */
-	{
-		printf("-> [%p] %d\n", (void *)array[index], array[index]->n);
-	}
-	free(array);
-	return (count);
+	(bool == 1) ? printf("-> [%p] %d\n", (void *)temp, temp->n) : (int)bool;
+	return (i);
 }
