855de52cd36c7904bd90421a70f9506fea314944
CalmEnduring <dantelm22@gmail.com>
2023-04-04 03:23:57 -0700

    add node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,36 +9,42 @@
   */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current = *head; /* current used to traverse list */
-	listint_t *newnode = malloc(sizeof(listint_t));
+	listint_t *current;/* current used to traverse list */
+	listint_t *newnode;
 	unsigned int i = 0; /* iterator */
-
-	if (newnode == NULL) /* malloc failure */
-		return (NULL);
-
-	newnode->n = n; /* initialize newnode data */
-	newnode->next = NULL; /* initialize next pointer to NULL */
 
 	if (*head != NULL) /* if head not EMPTY */
 	{
-		for (; i < idx - 1; i++) /* travere the list */
-		{
-			current = current->next;
-		}
-
-		if (current == NULL)
-		{
+		newnode = malloc(sizeof(listint_t)); /* allocate space for newnode */
+		if (newnode == NULL) /* malloc failure */
 			return (NULL);
-		}
-		else
-		{
-			/* add newnode */
-			newnode->next = current->next;
-			current->next = newnode;
-		}
 
+		newnode->n = n; /* initialize newnode data */
+		if (idx > 0) /* if index is not at the begining */
+		{
+			current = *head; /* current points to haed node */
+			while (current) /* travere the list */
+			{
+				if (i == idx) /* index found */
+				{
+					/* add newnode */
+					newnode->next = current->next;
+					current->next = newnode;
+					return (newnode); /* return newnode */
+				}
+				current = current->next; /* move to next node */
+				i++; /* increment */
+			}
+			if (idx > i) /* index not found */
+				return (NULL);
+		}
+		else /* index at the beginning */
+		{
+			/* add newnode to the beginning */
+			newnode->next = *head;
+			*head = newnode;
+		}
 		return (newnode); /* return newnode */
 	}
 	return (NULL); /* failure */
-
 }
