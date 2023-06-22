362f00c87f365ac3a61258a724fe9b9c6afd1eb6
CalmEnduring <dantelm22@gmail.com>
2023-06-22 14:33:11 -0700

    header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,5 +9,20 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void pac(char *separator, va_list args);
+void pai(char *separator, va_list args);
+void paf(char *separator, va_list args);
+void pacp(char *separator, va_list args);
+
+/**
+ * struct format_types - Struct format_types
+ * @identifier: The conversion specifier
+ * @f: The function pointer
+ */
+typedef struct dtypes
+{
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} f_data;
 
 #endif
