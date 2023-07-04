8a76185489be39a1267f5b60d9792cc2f5b9c287
CalmEnduring <dantelm22@gmail.com>
2023-07-04 01:44:02 -0700

    adds new head to linked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_nodeint - adds a new node at beginning of listint_t list
+ * @head: head node
+ * @n: element data
+ *
+ * Return: address to new element or NULL on failure
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newhead; /* newhead node */
+
+	if (head != NULL && n >= 0) /* head not empty and n not less than 0 */
+	{
+		newhead = malloc(sizeof(listint_t)); /* allocate space for newhead */
+		if (newhead == NULL) /* malloc failure */
+			return (NULL);
+
+		newhead->n = n; /* assign newhead element data */
+		newhead->next = *head; /* newhead points to old head */
+
+		*head = newhead; /* newhead assigned to old head */
+		return (newhead); /* return address of newhead */
+	}
+	return (0);
+}
