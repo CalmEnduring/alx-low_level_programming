e0ca5c5461c0dcd6bff1a6144724a13a325c4226
CalmEnduring <dantelm22@gmail.com>
2023-07-05 10:52:17 -0700

    check for palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+int check_drome(char *s);
+
+/**
+  * is_palindrome - Returns if a string is palindrome
+  * @s: the string value to be checked
+  *
+  * Return: integer value
+  */
+int is_palindrome(char *s)
+{
+	if (*s == '0') /* error */
+		return (1);
+
+	return (check_palindrome(s));
+}
+
+/**
+  * check_drome - Check if a string is palindrome
+  * @s: string to check
+  *
+  * Return: integer value
+  */
+int check_drome(char *s)
+{
+	/* lengh of string excluding null terminator */
+	int l = _strlen_recursion(s) - 1;
+
+	if (*s == s[l]) /* checking if string halves are identical */
+	{
+		s++; /* move forward to next element in first half of string */
+		l--; /* move backwards to next element in second half of string */
+	}
+	else /* no palindrome */
+	{
+		return (0);
+	}
+
+	return (1);
+}
+
+/**
+  * _strlen_recursion - Get the length of a string
+  * @s: the string to get the length
+  *
+  * Return: the string length
+  */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
