4dfdce9110761914c27946b6e0277c33eabda464
CalmEnduring <dantelm22@gmail.com>
2023-07-05 11:22:57 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -20,8 +20,8 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	*(&(*(p + 2)) - 2) = 98;
-	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", *(p + 7) - 926);
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
