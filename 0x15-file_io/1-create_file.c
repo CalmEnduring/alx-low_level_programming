36fa08a61188f342d354b6f1d76541ddf2a8fe1e
Calm Enduring <dantelm22@gmail.com>
2023-09-04 12:31:06 -0700

    function and header

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,21 +9,34 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, c = 0;
+	int fd;
 
 	if (!filename)
 		return (-1);
-
-	while (text_content[c])
-		text_content++;
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
-		write(fd, text_content, c);
+		write(fd, text_content, _strlen(text_content));
 
 	close(fd);
 	return (1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
