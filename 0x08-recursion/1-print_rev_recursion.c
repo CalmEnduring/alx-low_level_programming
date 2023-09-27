f0baf4b200422e09c6bc4177eaf3ce54fe592b0d
CalmEnduring <dantelm22@gmail.com>
2023-09-28 00:47:37 +0200

    print string in reversion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,14 +9,13 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0') /* base case */
 	{
-		/* new line at the end of string */
-		return; /* exit function*/
+		return; /* exit function */
 	}
 	else
 	{
 		s++; /* iterate to next element */
-		_print_rev_recursion(s); /* print the next element */
-		s--;
+		_print_rev_recursion(s); /* call function to the stack */
+		s--; /* move one element low */
 		_putchar(*s); /* print element */
 	}
 }
